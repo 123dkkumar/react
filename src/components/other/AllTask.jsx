d76645c2783a6e01ffb6bd64412619dafef85f98
123dkkumar <deepakkumar.258105@gmail.com>
2025-02-26 23:45:00 +0530

    work at admindshboard

---
+++ src/components/other/AllTask.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+const AllTask = () => {
+  return (
+    <div className="bg-[#1c1c1c] p-5 mt-5 rounded h-42 overflow-auto">
+      <div className="bg-red-400 py-2 mb-2  px-4 flex justify-between rounded">
+        <h2>Deepak</h2>
+        <h3>UI Design</h3>
+        <h5>Status</h5>
+      </div>
+      <div className="bg-green-400 py-2 mb-2 px-4 flex justify-between rounded">
+        <h2>Deepak</h2>
+        <h3>UI Design</h3>
+        <h5>Status</h5>
+      </div>
+      <div className="bg-pink-400 py-2 mb-2 px-4 flex justify-between rounded">
+        <h2>Deepak</h2>
+        <h3>UI Design</h3>
+        <h5>Status</h5>
+      </div>
+      <div className="bg-yellow-400 py-2 mb-2 px-4 flex justify-between rounded">
+        <h2>Deepak</h2>
+        <h3>UI Design</h3>
+        <h5>Status</h5>
+      </div>
+      <div className="bg-teal-400 py-2 mb-2 px-4 flex justify-between rounded">
+        <h2>Deepak</h2>
+        <h3>UI Design</h3>
+        <h5>Status</h5>
+      </div>
+    </div>
+  );
+};
+
+export default AllTask;
